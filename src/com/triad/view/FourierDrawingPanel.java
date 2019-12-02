88865af27aed0e0ac87a68b34bb8664e0341958f
Armin Zavada <armin.zavada@incquerylabs.com>
2019-12-02 05:37:46 +0100

    Implemented SVG file opening.

--- src/com/triad/view/FourierDrawingPanel.java
+++ src/com/triad/view/FourierDrawingPanel.java
@@ -9,31 +9,30 @@ import java.awt.geom.Line2D;
 import java.util.ArrayList;
 
 public class FourierDrawingPanel extends JPanel {
-    private FourierFunction fourierFunction;
-    int time = 0;
-    ArrayList<Complex> complexes = new ArrayList<>();
-    Timer timer;
+    private final FourierFunction fourierFunction;
+    private final ArrayList<Complex> complexes = new ArrayList<>();
+    private final Complex center;
+    private final float animationDuration;
+    private int time = 0;
+    private Timer timer;
 
-    public FourierDrawingPanel(FourierFunction fourierFunction) {
+    public FourierDrawingPanel(FourierFunction fourierFunction, int width, int height, float animationDuration) {
         this.fourierFunction = fourierFunction;
+        this.animationDuration = animationDuration;
         fourierFunction.setOnUpdateHandler(this::updateComplexFunction);
+        center = new Complex(width / 2f, height / 2f);
+
+        this.setSize(width, height);
+        this.setBackground(Color.GRAY);
     }
 
-    void updateComplexFunction() {
-        if (timer != null) timer.stop();
-        timer = new Timer((int)(3000f / fourierFunction.getMaxTime()), (ActionEvent) -> {
-            time++;
-            time %= fourierFunction.getMaxTime();
-
-            this.repaint();
-        });
-        timer.setRepeats(true);
-        timer.start();
+    private void updateComplexFunction() {
+        restartTimer();
 
         complexes.clear();
 
         for (int time = 0; time < fourierFunction.getMaxTime(); time++) {
-            Complex c = new Complex(0, 0);
+            Complex c = Complex.multiply(center, -1);
 
             for (int k = -fourierFunction.getLength(); k <= fourierFunction.getLength(); k++) {
                 c = Complex.add(c, fourierFunction.getValueAt(k, time));
@@ -43,17 +42,30 @@ public class FourierDrawingPanel extends JPanel {
         }
     }
 
-    void drawLine(Graphics2D g, Complex c1, Complex c2) {
+    private void restartTimer() {
+        if (timer != null) timer.stop();
+        timer = new Timer((int)(animationDuration / fourierFunction.getMaxTime()), (ActionEvent) -> {
+            time++;
+            time %= fourierFunction.getMaxTime();
+
+            this.repaint();
+        });
+
+        timer.setRepeats(true);
+        timer.start();
+    }
+
+    private void drawLine(Graphics2D g, Complex c1, Complex c2) {
         g.drawLine((int)c1.getReal(), (int)c1.getImaginary(), (int)c2.getReal(), (int)c2.getImaginary());
     }
 
     private void drawFourier(Graphics2D g) {
-        Complex c1 = new Complex(0, 0);
-        Complex c2 = Complex.add(c1, Complex.multiply(fourierFunction.getValueAt(0, time), -1));
+        if (fourierFunction.getLength() == 0) return;
+
+        Complex c1 = Complex.Zero;
+        Complex c2 = center;
 
         drawLine(g, c1, c2);
-
-        var line = new Line2D.Float();
 
         for (int i = 1; i <= fourierFunction.getLength(); i++) {
             c1 = Complex.add(c2, Complex.multiply(fourierFunction.getValueAt(i, time), -1));
--- src/com/triad/view/MainWindow.java
+++ src/com/triad/view/MainWindow.java
@@ -11,8 +11,8 @@ import java.net.URISyntaxException;
 
 public class MainWindow extends JFrame {
     JFileChooser chooser = new JFileChooser();
-    ComplexFunction function = new SVGComplexFunction(new URI("/home/armin/test.svg"), 1000);
-    ComplexSeriesProvider seriesProvidier = new ComplexSeriesProviderImplementation(50);
+    ComplexFunction function = new SVGComplexFunction(new URI("/home/armin/test.svg"), 100);
+    ComplexSeriesProvider seriesProvidier = new ComplexSeriesProviderImplementation(10);
     FourierFunction seriesFunction = new FourierTemporalPointContainer();
 
     public MainWindow() throws IOException, URISyntaxException {
@@ -20,10 +20,45 @@ public class MainWindow extends JFrame {
         seriesFunction.setComplexSeriesProvider(seriesProvidier);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        var function = new SVGComplexFunction(new URI("/home/armin/test.svg"), 1000);
-        var seriesProvidier = new ComplexSeriesProviderImplementation(function, 30);
-        var seriesFunction = new FourierTemporalPointContainer(seriesProvidier);
-        this.add(new FourierDrawingPanel(seriesFunction));
-        this.pack();
+        var filter = new FileNameExtensionFilter("SVG files", "svg");
+        chooser.setFileFilter(filter);
+        initMenu();
+        var panel = new JPanel();
+        panel.setBackground(Color.RED);
+        this.add(new FourierDrawingPanel(seriesFunction, 600, 600, 3000));
+        this.setSize(600, 600);
+    }
+
+    private void openLoadFileDialog() {
+        if(chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            try {
+                seriesProvidier.setComplexFunction(new SVGComplexFunction(chooser.getSelectedFile().toURI(), 1000));
+            }
+            catch (IOException e) {
+                 JOptionPane.showMessageDialog(this, "SVG file could not be loaded properly.", "File error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void openSaveFileDialog() {
+
+    }
+
+    private void initMenu() {
+        var menuBar = new MenuBar();
+        var menu = new Menu("File");
+
+        var saveMenuItem = new MenuItem("Load");
+        saveMenuItem.getAccessibleContext().setAccessibleDescription("Loads a new SVG file to display.");
+        saveMenuItem.addActionListener(actionEvent -> openLoadFileDialog());
+
+        var loadMenuItem = new MenuItem("Save");
+        loadMenuItem.getAccessibleContext().setAccessibleDescription("The only menu in this program that has menu items");
+        loadMenuItem.addActionListener(actionEvent -> openSaveFileDialog());
+
+        menu.add(saveMenuItem);
+        menu.add(loadMenuItem);
+        menuBar.add(menu);
+        this.setMenuBar(menuBar);
     }
 }
